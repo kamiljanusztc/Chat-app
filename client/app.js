0269f4edca792acba76c847d06d0117365eca93a
Kamil Janusz <kamiljanusztc@gmail.com>
2021-05-06 20:02:25 +0200

    Add addMessage function to app.js

--- client/app.js
+++ client/app.js
@@ -8,6 +8,7 @@ const messageContentInput = document.getElementById('message-content');
 let userName = '';
 
 loginForm.addEventListener('submit', event => login(event));
+addMessageForm.addEventListener('submit', event => sendMessage(event));
 
 const login = (event) => {
   event.preventDefault();
@@ -17,5 +18,31 @@ const login = (event) => {
     messagesSection.classList.add('show');
   } else {
     window.alert('This field is empty!');
+  }
+};
+
+const sendMessage = (event) => {
+  event.preventDefault();
+  if (messageContentInput.value) {
+    addMessage(userName, messageContentInput.value);
+  } else {
+    window.alert('This field is empty!');
+  }
+};
+
+const addMessage = (author, content) => {
+  const message = document.createElement('li');
+  message.classList.add('message');
+  message.classList.add('message--received');
+
+  if (author === userName) {
+    message.classList.add('message--self');
+    message.innerHTML = `
+      <h3 class="message__author">${userName === author ? 'You' : author }</h3>
+      <div class="message__content">
+        ${content}
+      </div>
+    `;
+    messagesList.appendChild(message);
   }
 };
